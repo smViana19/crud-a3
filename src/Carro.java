3a37029c9e3abcded526a41fe16bb7c133e623f1
Samuel <samuelfilipevianadois@gmail.com>
2025-05-21 16:33:41 -0300

    add classe principal

---
+++ src/Carro.java
@@ -0,0 +1,55 @@
+public class Carro {
+    private int id;
+    private String nome;
+    private String cor;
+    private String ano;
+    private String marca;
+
+    public Carro(int id, String nome, String cor, String ano, String marca) {
+        this.id = id;
+        this.nome = nome;
+        this.cor = cor;
+        this.ano = ano;
+        this.marca = marca;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCor() {
+        return cor;
+    }
+
+    public void setCor(String cor) {
+        this.cor = cor;
+    }
+
+    public String getAno() {
+        return ano;
+    }
+
+    public void setAno(String ano) {
+        this.ano = ano;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,34 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        Scanner scanner = new Scanner(System.in);
+        int opcao;
+        do {
+            System.out.println("***** MENU CRUD *****");
+            System.out.println("1 - Salvar");
+            System.out.println("2 - Listar");
+            System.out.println("3 - Editar");
+            System.out.println("4 - Excluir");
+            System.out.println("0 - Sair");
+            System.out.print("Escolha uma opção: ");
+            opcao = scanner.nextInt();
+            switch (opcao) {
+                case 1:
+                    System.out.println("Logica para salvar");
+                case 2:
+                    System.out.println("Logica para lsitar");
+                case 3:
+                    System.out.println("Logica para Editar");
+                case 4:
+                    System.out.println("Logica para Excluir");
+                default:
+                    System.out.println("Opção invalida");
+            }
+        } while (opcao != 0);
+        {
+            scanner.close();
+
+        }
     }
 }
